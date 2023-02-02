ce69253b5c6ab115c63d6e60acb52f1524fd9603
Jeffrey Song <jeffsong831@gmail.com>
2023-02-01 22:48:53 -0800

    analyze NYC squirrel data

--- day-25-csv-and-pandas/main.py
+++ day-25-csv-and-pandas/main.py
@@ -14,34 +14,52 @@
 
 import pandas
 
-data = pandas.read_csv("weather_data.csv")
+# data = pandas.read_csv("weather_data.csv")
+# # # print(data["temp"])
+# #
+# # data_dict = data.to_dict()
+# # print(data_dict)
+# #
+# # temp_list = data["temp"].to_list()
+# # print(temp_list)
+# #
+# # print(data["temp"].mean())
+# #
+# # # get data in column
+# # print(data.temp)
 # # print(data["temp"])
+# #
+# # get data in row
+# # print(data[data.temp == data.temp.max()])
 #
-# data_dict = data.to_dict()
-# print(data_dict)
+# # tuesday = data[data.day == "Tuesday"]
+# # tuesday_temp = int(tuesday.temp)
+# # print(tuesday.temp)
 #
-# temp_list = data["temp"].to_list()
-# print(temp_list)
+# #create a dataframe
+# data_dict = {
+#     "students": ["Aloe", "Khoi", "Jeffrey"],
+#     "scores": [0, 100, 97]
+# }
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("test_scores.csv")
 #
-# print(data["temp"].mean())
-#
-# # get data in column
-# print(data.temp)
-# print(data["temp"])
-#
-# get data in row
-# print(data[data.temp == data.temp.max()])
 
-# tuesday = data[data.day == "Tuesday"]
-# tuesday_temp = int(tuesday.temp)
-# print(tuesday.temp)
+squirrel_data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+color_list = squirrel_data["Primary Fur Color"].to_list()
+colors = [0, 0, 0]
+for color in color_list:
+    if color == "Gray":
+        colors[0] += 1
+    elif color == "Cinnamon":
+        colors[1] += 1
+    elif color == "Black":
+        colors[2] += 1
 
-#create a dataframe
 data_dict = {
-    "students": ["Aloe", "Khoi", "Jeffrey"],
-    "scores": [0, 100, 97]
+    "Fur Color": ["Gray", "Red", "Black"],
+    "Count": [colors[0], colors[1], colors[2]]
 }
 data = pandas.DataFrame(data_dict)
-data.to_csv("test_scores.csv")
-
+data.to_csv("squirrel_count.csv")
 
