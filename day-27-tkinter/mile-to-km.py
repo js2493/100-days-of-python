4d60b6c97a34c5872ddfa13f9a52ae3a4e7b92d4
Jeffrey Song <jeffsong831@gmail.com>
2023-02-10 22:41:38 -0800

    finish miles to KM converter

--- day-27-tkinter/mile-to-km.py
+++ day-27-tkinter/mile-to-km.py
@@ -2,10 +2,9 @@ from tkinter import *
 
 window = Tk()
 window.title("Mile to KM Converter")
-window.minsize(width=500, height=300)
-window.config(padx=100, pady=200)
+window.config(padx=25, pady=25)
 
-num_input = Entry()
+num_input = Entry(width=5)
 num_input.grid(row=0, column=1)
 
 text1 = Label(text="Miles", font=("Arial", 16))
@@ -22,7 +21,7 @@ text4.grid(row=1, column=2)
 
 
 def button_clicked():
-    value = int(num_input.get())
+    value = float(num_input.get())
     value = value * 1.60934
     text3["text"] = value
 
