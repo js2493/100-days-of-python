85f6b2505bab99d18662900201b3a56874b03301
Jeffrey Song <jeffsong831@gmail.com>
2023-02-11 00:21:59 -0800

    add working Pomodoro timer with start and reset buttons

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -9,12 +9,58 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
+pause = False
+timer = None
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    window.after_cancel(timer)
+    reps = 0
+    timer_label.config(text="Timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    check_marks.config(text="")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    if reps % 2 == 1:
+        timer_label.config(text="Work", fg=GREEN)
+        count_down(WORK_MIN * 60)
+    elif reps == 8:
+        timer_label.config(text="Break", fg=RED)
+        count_down(LONG_BREAK_MIN * 60)
+    else:
+        timer_label.config(text="Break", fg=PINK)
+        count_down(SHORT_BREAK_MIN * 60)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    global pause
+    global reps
+    seconds = count % 60
+    minutes = int(count / 60)
+    time = ""
+    if seconds < 10:
+        time = f"{minutes}:0{seconds}"
+    else:
+        time = f"{minutes}:{seconds}"
+    canvas.itemconfig(timer_text, text=time)
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    elif reps < 8:
+        start_timer()
+        mark = ""
+        for n in range(int(reps/2)):
+            mark += "✔"
+        check_marks.config(text=mark)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -22,22 +68,22 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-title_label = Label(text="Timer", fg=GREEN, font = (FONT_NAME, 50), bg=YELLOW)
-title_label.grid(row=0, column=1)
+timer_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
+timer_label.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(row=1, column=1)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks = Label(text="", fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 window.mainloop()
