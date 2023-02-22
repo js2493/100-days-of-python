6a40283bc9a4211406249c0ea9f4065a4b4a8685
Jeffrey Song <jeffsong831@gmail.com>
2023-02-21 21:14:33 -0800

    practice with smtplib

---
+++ birthday-wisher/main.py
@@ -0,0 +1,23 @@
+import smtplib
+import datetime as dt
+import random
+
+email = "email"
+password = "password"
+now = dt.datetime.now()
+weekday = now.weekday()
+
+if weekday == 1:
+    with open("quotes.txt") as quotes:
+        quote_list = quotes.readlines()
+        quote = random.choice(quote_list)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=email, password=password)
+        connection.sendmail(
+            from_addr=email,
+            to_addrs="to_email",
+            msg="Subject:Happy Tuesday!\n\n" + quote)
+
+
